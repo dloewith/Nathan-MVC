dd1b448784b95e3778189f0043b85b4f4b72234a
Nathan Davison <ndavison85@gmail.com>
2012-07-14 23:56:52 +1000

    V1.11

--- classes/basemodel.php
+++ classes/basemodel.php
@@ -14,7 +14,15 @@ class BaseModel {
     public function __construct()
     {
         $this->viewModel = new ViewModel();
+		
+		$this->commonViewData();
     }
+	
+	//establish viewModel data that is required for all views in this method (i.e. the main template)
+    protected function commonViewData() {
+	
+		//e.g. $this->viewModel->set("mainMenu",array("Home" => "/home", "Help" => "/help"));
+	}
 }
 
 ?>
